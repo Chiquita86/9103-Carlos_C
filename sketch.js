082022eee8003ba5d43da85736c60c2bf086c189
YolisZeng <yzen0964@uni.sydney.edu.au>
2025-06-05 16:54:43 +1000

    First Attempt
    Yulin Zeng
    The first attempt: creating concentric circles

--- sketch.js
+++ sketch.js
@@ -1,7 +1,28 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 600);
+  noLoop();
 }
 
 function draw() {
-  background(220);
+  background(255);
+
+  let gridSize = 100
+  let layers = 8;
+  let maxRadius = 40;
+
+  for(let y = gridSize / 2; y < height; y+= gridSize){
+    for(let x = gridSize / 2; x < width; x+= gridSize){
+      drawConcentricCircle(x, y, layers, maxRadius);
+    }
+  }
+}
+
+function drawConcentricCircle(cx, cy, numLayers, maxRadius){
+  for(let i = numLayers; i > 0; i--) {
+    let radius = (i/numLayers) * maxRadius;
+
+    fill((random(50, 255)), random(20, 255), random(20, 255));
+    noStroke();
+    ellipse(cx, cy, radius*2, radius*2);
+  }
 }
